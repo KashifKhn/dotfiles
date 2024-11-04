368317b6d81a8d74ee6c827c9e19db4feb989759
KashifKhn <kashifkhnx04@gmail.com>
2024-11-04 23:29:59 +0500

    disable arrow key in all modes

--- nvim/.config/nvim/lua/customs/core/keymaps.lua
+++ nvim/.config/nvim/lua/customs/core/keymaps.lua
@@ -27,3 +27,9 @@ keymap.set("n", "<leader>wl", "<C-W>l", { desc = "Move to window right", remap =
 keymap.set("n", "<leader>-", "<C-W>s", { desc = "Split Window Below", remap = true })
 keymap.set("n", "<leader>|", "<C-W>v", { desc = "Split Window Right", remap = true })
 keymap.set("n", "<leader>wd", "<C-W>c", { desc = "Delete Window", remap = true })
+
+-- Disable arrow keys in all modes
+keymap.set({ "n", "i", "v" }, "<Up>", "<Nop>", { desc = "Disable Up Arrow" })
+keymap.set({ "n", "i", "v" }, "<Down>", "<Nop>", { desc = "Disable Down Arrow" })
+keymap.set({ "n", "i", "v" }, "<Left>", "<Nop>", { desc = "Disable Left Arrow" })
+keymap.set({ "n", "i", "v" }, "<Right>", "<Nop>", { desc = "Disable Right Arrow" })
