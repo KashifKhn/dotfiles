223135c524d68398c610ae912766849142947dc2
KashifKhn <kashifkhnx04@gmail.com>
2025-06-18 10:50:40 +0500

    fix java lombok settings

--- nvim/.config/nvim/lua/customs/plugins/jdtls.lua
+++ nvim/.config/nvim/lua/customs/plugins/jdtls.lua
@@ -11,10 +11,29 @@ return {
 
 		local home = os.getenv("HOME")
 		local workspace_folder = home .. "/.local/share/eclipse/" .. vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
+		local lombok_path = home .. "/.local/share/nvim/mason/packages/jdtls/lombok.jar"
 
 		local config = {
 			cmd = {
-				"jdtls",
+				"java",
+				"-Declipse.application=org.eclipse.jdt.ls.core.id1",
+				"-Dosgi.bundles.defaultStartLevel=4",
+				"-Declipse.product=org.eclipse.jdt.ls.core.product",
+				"-Dlog.protocol=true",
+				"-Dlog.level=ALL",
+				"-javaagent:" .. lombok_path,
+				"-Xms1g",
+				"--add-modules=ALL-SYSTEM",
+				"--add-opens",
+				"java.base/java.util=ALL-UNNAMED",
+				"--add-opens",
+				"java.base/java.lang=ALL-UNNAMED",
+				"-jar",
+				vim.fn.glob(
+					home .. "/.local/share/nvim/mason/packages/jdtls/plugins/org.eclipse.equinox.launcher_*.jar"
+				),
+				"-configuration",
+				home .. "/.local/share/nvim/mason/packages/jdtls/config_linux",
 				"-data",
 				workspace_folder,
 			},
